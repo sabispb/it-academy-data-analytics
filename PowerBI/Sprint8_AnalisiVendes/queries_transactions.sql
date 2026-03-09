a0fb416be395fc18b27577e203001447873216a9
sabina <sabinaplanas@gmail.com>
2026-03-09 13:58:17 +0100

    added sql queries

--- PowerBI/Sprint8_AnalisiVendes/queries_transactions.sql
+++ PowerBI/Sprint8_AnalisiVendes/queries_transactions.sql
@@ -19,3 +19,30 @@ FROM transactions
 WHERE declined = '1'
 GROUP BY YEAR(transaction_date)
 ORDER BY YEAR(transaction_date) ASC;
+
+
+SELECT COUNT(product_id), YEAR(transaction_date)
+FROM transaction_product tp
+JOIN transactions t ON tp.transaction_id = t.id
+GROUP BY YEAR(transaction_date);
+
+SELECT MIN(price), MAX(price)
+FROM products p
+JOIN transaction_product tp ON p.id = tp.product_id
+JOIN transactions t ON tp.transaction_id = t.id
+GROUP BY YEAR(transaction_date);
+
+SELECT ROUND(SUM(amount), 2), YEAR(transaction_date) AS year, country
+FROM transactions t
+JOIN all_users u ON u.id = t.user_id
+WHERE country IN ('Poland','France')
+	AND YEAR(transaction_date) IN ('2020','2024')
+GROUP BY YEAR(transaction_date), country
+order by YEAR(transaction_date);
+
+SELECT COUNT(p.id), YEAR(transaction_date), transaction_id
+FROM products p
+JOIN transaction_product tp ON p.id = tp.product_id
+JOIN transactions t ON t.id = tp.transaction_id
+-- ORDER BY transaction_id
+GROUP BY transaction_id;
